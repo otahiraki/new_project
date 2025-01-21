197473ac6d339ae89d12115ad675d37b05b0f284
大田開 <otahiraki@ootakainonotobukkukonpyuta.local>
2025-01-21 10:47:38 +0900

    mainおよびGame.javaの修正

--- src/main/java/jp/ac/uryukyu/ie/e245733/Game.java
+++ src/main/java/jp/ac/uryukyu/ie/e245733/Game.java
@@ -1,11 +1,11 @@
 package main.java.jp.ac.uryukyu.ie.e245733;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.Random; //ランダムの数を取得するjava機能
+import java.util.Scanner; //数値入力するためのjava機能
 
 public class Game {
-    private int targetNumber;
-    private int attempts;
+    private int targetNumber; //当てるべき数値
+    private int attempts; //クリアまでの挑戦回数
     private final Scanner scanner = new Scanner(System.in);
 
     // ゲームを開始するメソッド
@@ -33,8 +33,8 @@ public class Game {
             int guess = scanner.nextInt();
             attempts++;
 
-            if (guess == targetNumber) {
-                System.out.printf("正解です！%d回の試行で当てました。\n", attempts);
+            if (guess == targetNumber)  { 
+                System.out.printf("正解です！%d回の試行で当てました。おめでとうございます！！！\n", attempts);
                 break; // ループを終了する
             } else if (guess < targetNumber) {
                 System.out.println("もっと大きい数字です。");
---
+++ src/main/java/jp/ac/uryukyu/ie/e245733/Main.java
@@ -0,0 +1,8 @@
+package main.java.jp.ac.uryukyu.ie.e245733;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game(); // ゲームオブジェクトの作成
+        game.start(); // ゲームを開始するメソッドをGame.javaから呼び出す
+    }
+}
